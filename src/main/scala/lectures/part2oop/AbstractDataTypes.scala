2187014ea327425a5ff11e1005ed1d3b50393ddf
Johan Loke <johan.loke@lendlease.com>
2022-10-12 16:56:14 +0800

    check in

---
+++ src/main/scala/lectures/part2oop/AbstractDataTypes.scala
@@ -0,0 +1,39 @@
+package lectures.part2oop
+
+object AbstractDataTypes extends App{
+  abstract class Animal {
+    val creatureType: String = "wild"
+    def eat: Unit
+  }
+
+  class Dog extends Animal {
+    override val creatureType: String = "Canine"
+
+    def eat: Unit = println("crunch crunch")
+  }
+
+  // traits
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  trait ColdBlooded {
+    def eat(animal: Animal): Unit
+    val preferredMeal: String = "fresh meat"
+  }
+
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val creatureType: String = "croc"
+    def eat: Unit = println("nomnomnom")
+    def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}")
+  }
+
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
+
+  // traits vs abstract
+  // 1 - traits do not have constructor parameters
+  // 2 - multiple traits may be inherited by the same class
+  // 3 - traits = behavior, abstract class = "thing"
+}
