651f724a164ba385f8194f616905d998b06198e8
Johan Loke <johan.loke@lendlease.com>
2022-10-19 17:13:34 +0800

    check in

--- src/main/scala/lectures/part4pm/PatternsEverywhere.scala
+++ src/main/scala/lectures/part4pm/PatternsEverywhere.scala
@@ -1,5 +1,66 @@
 package lectures.part4pm
 
-class PatternsEverywhere extends App {
+object PatternsEverywhere extends App {
 
+  // big ideas #1
+  try {
+    // code
+  } catch {
+    case e: RuntimeException => "runtime"
+    case npe: NullPointerException => "npe"
+    case _ => "something else"
+  }
+
+  // catches are actually MATCHES
+  /*
+    try {
+      //code
+    } catch(e) {
+      case e: RuntimeException => "runtime"
+      case npe: NullPointerException => "npe"
+      case _ => "something else"
+    }
+   */
+
+  // big idea #2
+  val list = List(1, 2, 3, 4)
+  val evenOnes = for {
+    x <- list if x % 2 == 0 //?!
+  } yield 10 * x
+
+  //generators are also based on PATTERN MATCHING
+  val tuples = List((1, 2), (3, 4))
+  val filterTuples = for {
+    (first, second) <- tuples
+  } yield first * second
+  //case classes, :: operators. ...
+
+  // big idea #3
+  val tuple = (1, 2, 3)
+  val (a, b, c) = tuple
+  println(b)
+  // multiple value definiations based on PATTERN MATCHING
+  // ALL THE POWER
+
+  val head :: tail = list
+  println(head)
+  println(tail)
+
+  //big idea #4 - NEW
+  // partial function based on PATTERN MATCHING
+  val mappedList = list.map {
+    case v if v % 2 == 0 => v + " is even"
+    case 1 => "the one"
+    case _ => "something else"
+  } // partial function literal
+
+  val mappedListExpanded = list.map {
+    x =>
+      x match {
+        case v if v % 2 == 0 => v + " is even"
+        case 1 => "the one"
+        case _ => "something else"
+      }
+  }
+  println(mappedListExpanded)
 }
