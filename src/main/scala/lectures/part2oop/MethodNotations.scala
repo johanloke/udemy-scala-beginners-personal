c25b2ebd08443f461ceb2634f811c78b030129d1
Johan Loke <johan.loke@lendlease.com>
2022-10-24 13:28:50 +0800

    check in

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -4,19 +4,29 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String, val age: Int = 0){
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
+
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+
     def +(person: Person): String = s"${this.name} plus ${person.name}"
+
     //+ is not a reserved word for method
     def unary_! : String = s"$name. what the heck!"
+
     def isAlive: Boolean = true
+
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+
     //if an object is called directly, it will look for apply()
     def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+
     def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+
     def learns(course: String): String = s"$name learn $course"
+
     def learnsScala: String = this.learns("Scala")
+
     def apply(number: Int): String = s"$name watched $favoriteMovie $number times"
 
   }
@@ -30,7 +40,7 @@ object MethodNotations extends App {
   // "operators" in Scala
   val tom = new Person("Tom", "Fight Club")
   println(mary hangOutWith tom)
-  println(mary hangOutWith(tom))
+  println(mary hangOutWith (tom))
   println(mary + tom)
 
   println(1 + 2)
@@ -41,7 +51,7 @@ object MethodNotations extends App {
   //prefix notation
   val x = -1 // equivalent with 1.unary_-
   val y = 1.unary_-
-  // unary_ prefix only works with - + - !
+  // unary_ prefix only works with - + ~ !
 
   println(!mary)
   println(mary.unary_!)
@@ -57,19 +67,19 @@ object MethodNotations extends App {
 
   /**
    * 1. Overload the + operator
-   *    mary + "the rockstart" => new person "Mary (the rockstar)"
+   * mary + "the rockstart" => new person "Mary (the rockstar)"
    *
    * 2. Add an age to the Person class
-   *    Add a unary + operator => new person with the age + 1
-   *    +mary => mary with the age incrementer
+   * Add a unary + operator => new person with the age + 1
+   * +mary => mary with the age incrementer
    * 3. Add a "learns" method in the Person class => "Mary learns Scala"
-   *    Add a learnsScala method, calls learns method with "Scala"
-   *    Use it in postfix notation.
+   * Add a learnsScala method, calls learns method with "Scala"
+   * Use it in postfix notation.
    * 4. Overload the apply method
-   *    mary.apply(2) => "Mary watched Inception 2 times"
+   * mary.apply(2) => "Mary watched Inception 2 times"
    */
 
-  println((mary + "the Rockstar")())
+  println((mary + "the Rockstar") ())
 
   println((+mary).age)
 
